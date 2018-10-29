801919bdd7c9473bca66ba9c76ab0bb08bb75368
jp052 <jan.p2001@gmail.com>
2018-10-29 23:59:45 +1030

    Add days endpoint and test partially

--- src/main/java/com/plankdev/datetimeapi/api/DateTimeRestController.java
+++ src/main/java/com/plankdev/datetimeapi/api/DateTimeRestController.java
@@ -1,14 +1,38 @@
 package com.plankdev.datetimeapi.api;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(value = "api/v1/datetime")
 @RestController
 public class DateTimeRestController {
+	
+	///api/v1/datetime/days?startDateTime=2018-09-05T05:30&endDateTime=2018-09-10T05:30
+	@GetMapping("/days")
+	public String readDays (
+						@RequestParam("startDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDateTime, 
+						@RequestParam("endDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDateTime,
+						@RequestParam(value = "startZone", required = false, defaultValue = "Australia/Adelaide") String startZone,
+						@RequestParam(value = "endZone", required = false, defaultValue = "Australia/Adelaide") String endZone,
+						@RequestParam(value = "format", required = false, defaultValue = "day") String format) {
+		
+		//check if endDate is younger than startdate
+		//create ZonedDateTime
+		//subtract startdate from endDate
+		//convert to correct format (sec, min, day, year)
+		
+		String timeBetweenDates = "";
+		
+		return timeBetweenDates;
+	}
 
-	@GetMapping(value = "/hello")
+	@GetMapping("/hello")
 	public String hello() {
 		return "{\"hello\":\"datetime api\"}";
 	}
--- src/test/java/com/plankdev/datetimeapi/api/DateTimeRestControllerTest.java
+++ src/test/java/com/plankdev/datetimeapi/api/DateTimeRestControllerTest.java
@@ -7,12 +7,45 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 import org.junit.Test;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 public class DateTimeRestControllerTest extends RestControllerTestBase {
 	private final static String ENDPOINT_BASE = "/api/v1/datetime";
+	
+	private final static String START_DATE = "startDateTime=";
+	private final static String END_DATE = "endDateTime=";
+	private final static String START_ZONE = "startZone=";
+	private final static String END_ZONE = "endZone=";
+	private final static String FORMAT = "format=";
+	
+	@Test
+	public void testReadDays() throws Exception {
+		//assemble
+		LocalDate startDate = LocalDate.of(2018, 9, 5);
+		LocalTime startTime = LocalTime.of(5, 30);
+		LocalDateTime startDateTime = LocalDateTime.of(startDate, startTime);
+		
+		LocalDate endDate = LocalDate.of(2018, 9, 10);
+		LocalTime endTime = LocalTime.of(5, 30);
+		LocalDateTime endDateTime = LocalDateTime.of(endDate, endTime);
+		
+		String queryURL = ENDPOINT_BASE + "/days?" + START_DATE + startDateTime.toString() + "&" + END_DATE + endDateTime.toString();
+		
+		//action
+		ResultActions readDaysResult = mockMvc.perform(get(queryURL));
+		
+		//assert
+		readDaysResult
+			.andExpect(status().is2xxSuccessful());
+			//.andExpect(jsonPath("$.days").value(5))
+			//.andExpect(jsonPath("$.format").value("day"));
+	}
 	
 	@Test
 	public void testHello() throws Exception {
